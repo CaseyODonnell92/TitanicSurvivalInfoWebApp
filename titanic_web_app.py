62d428e7a2d384a3bccb7ff3f0d0661d47cc825e
Casey O'Donnell <caseybrian92@gmail.com>
2016-10-09 13:27:41 +0200

    Working version with chart.js pie charts

--- titanic_web_app.py
+++ titanic_web_app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 import titanic_data
+import json
 
 app = Flask(__name__)
 
@@ -20,4 +21,10 @@ def hello_world():
 
 	return render_template('titanic_template.html', num_passengers = num_passengers, \
 													num_survivors = len(survivors), \
-													num_deceased=len(deceased))
+													num_deceased=len(deceased),
+													survAge = json.dumps(titanic_data.get_age_categories(survivors)),
+													survSex = json.dumps(titanic_data.get_gender(survivors)),
+													survClass = json.dumps(titanic_data.get_class(survivors)),
+													nonSurvAge = json.dumps(titanic_data.get_age_categories(deceased)),
+													nonSurvSex = json.dumps(titanic_data.get_gender(deceased)),
+													nonSurvClass = json.dumps(titanic_data.get_class(deceased)))
